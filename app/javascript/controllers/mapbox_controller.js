1ae82a7b46e218260796000dbdf0e4560f8cc41d
Nico Proto <nicolasproto@gmail.com>
2021-11-04 12:08:16 +0100

    FEAT: Added markers to map and fitToMarkers method

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -11,9 +11,28 @@ export default class extends Controller {
     if (this.mapTarget) {
       mapboxgl.accessToken = this.mapTarget.dataset.mapboxApiKey;
       const map = new mapboxgl.Map({
-        container: 'map',
+        container: this.mapTarget,
         style: 'mapbox://styles/mapbox/streets-v10'
       });
+
+      const markers = JSON.parse(this.mapTarget.dataset.markers);
+      markers.forEach((marker) => {
+        new mapboxgl.Marker()
+          .setLngLat([marker.lng, marker.lat])
+          .addTo(map);
+      });
+
+      this.fitMapToMarkers(map, markers);
     }
   }
+
+  fitMapToMarkers(map, markers) {
+    const bounds = new mapboxgl.LngLatBounds();
+    markers.forEach(marker => bounds.extend([marker.lng, marker.lat]));
+    map.fitBounds(bounds, {
+      padding: 70,
+      maxZoom: 15,
+      duration: 0
+    });
+  };
 }
